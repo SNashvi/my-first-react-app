bd18fa7dc1cf3c3df78efa5b2d759009ecfa1276
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-10 17:16:30 +1100

    My First React App - Use React.Fragment to return multiple elements

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
 function Greeting (){
-  return <h4>This is Shohreh and this is my first component</h4>
+  return (
+    <React.Fragment>
+      <article>
+        <h3>Hello World</h3>
+      </article>
+    </React.Fragment>
+  );
 }
+
+// const Greeting = () =>{
+//   return React.createElement('h1',{}, 'This is Shohreh and this is my first component');
+// }
 
 ReactDom.render(<Greeting/>,document.getElementById('root'));
