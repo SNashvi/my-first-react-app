a63a8b07793265d28d39e7330acde582465e7610
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-11 13:59:44 +1100

    My First React App - Create nested components.

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// JSX Rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
+// Nested Components, React Tools
 
 function Greeting (){
   return (
-    <React.Fragment>
-      <article>
-        <h3>Hello World</h3>
-      </article>
-    </React.Fragment>
+    <div>
+      <Person/>
+      <Message/>
+    </div>
   );
 }
 
-// const Greeting = () =>{
-//   return React.createElement('h1',{}, 'This is Shohreh and this is my first component');
-// }
+const Person = () => <h2>Shohreh</h2>
+const Message = () => {
+  return <p>This is my message</p>
+}
 
 ReactDom.render(<Greeting/>,document.getElementById('root'));
