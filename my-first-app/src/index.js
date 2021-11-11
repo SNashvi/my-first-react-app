8cd62fde19c74d6a3378ec6695396f3e8d9f1a73
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-11 16:28:10 +1100

    My First React App - Add Image, Title and Style to Book List App

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+
+//CSS
+import "./index.css";
 
 // Nested Components, React Tools
 
-function Greeting (){
+function BookList() {
   return (
-    <div>
-      <Person/>
-      <Message/>
-    </div>
+    <section className="booklist">
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>Shohreh</h2>
-const Message = () => {
-  return <p>This is my message</p>
-}
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-ReactDom.render(<Greeting/>,document.getElementById('root'));
+const Image = () => <img src="https://m.media-amazon.com/images/I/71f9a+-aSPL._AC_UL320_.jpg" alt="Teach Your Dragon To Share Book" />;
+
+const Title = () => <h1>Teach Your Dragon To Share</h1>;
+
+const Author = () => <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>by Steve Herman</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
