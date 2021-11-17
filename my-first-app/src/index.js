c46bdc25e44c01db8526c2a770d1f55be62d3f46
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-17 16:25:19 +1100

    My First React App - Two different ways to access properties of an object (First way has been commented out)

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -28,12 +28,13 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
+const Book = ({ img, title, author, alt }) => {
+  // const { img, title, author, alt } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.alt} />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt={alt} />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
