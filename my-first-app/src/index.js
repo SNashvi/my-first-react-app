1a3ee7c875f6d6521a1073b378e5e6d4c5cabdab
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-17 16:21:20 +1100

    My First React App - Create Object

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -4,23 +4,36 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-// Nested Components, React Tools
+// Setup vars
+const firsBook = {
+  author: "by Steve Herman",
+  title: "Teach Your Dragon To Share",
+  img: "https://m.media-amazon.com/images/I/71f9a+-aSPL._AC_UL320_.jpg",
+  alt: "Teach Your Dragon To Share Book",
+};
+
+const secondBook = {
+  author: "by Andy Griffiths and Terry Denton",
+  title: "The 143-Storey Treehouse",
+  img: "https://m.media-amazon.com/images/I/91fUxKC9POL._AC_UL320_.jpg",
+  alt: "The 143-Storey Treehouse",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book img={firsBook.img} title={firsBook.title} author={firsBook.author} alt={firsBook.alt} />
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} alt={secondBook.alt} />
     </section>
   );
 }
-const author = "by Steve Herman";
-const Book = () => {
-  const title = "Teach Your Dragon To Share";
+
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://m.media-amazon.com/images/I/71f9a+-aSPL._AC_UL320_.jpg" alt="Teach Your Dragon To Share Book" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt={props.alt} />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
