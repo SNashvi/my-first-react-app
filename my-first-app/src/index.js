3c2c2780510b7fc425a7a34631a06ff247073745
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-18 13:56:40 +1100

    My First React App - Setup Array with unique id and access it by map function

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -4,40 +4,48 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-// Setup vars
-const firsBook = {
-  author: "by Steve Herman",
-  title: "Teach Your Dragon To Share",
-  img: "https://m.media-amazon.com/images/I/71f9a+-aSPL._AC_UL320_.jpg",
-  alt: "Teach Your Dragon To Share Book",
-};
-
-const secondBook = {
-  author: "by Andy Griffiths and Terry Denton",
-  title: "The 143-Storey Treehouse",
-  img: "https://m.media-amazon.com/images/I/91fUxKC9POL._AC_UL320_.jpg",
-  alt: "The 143-Storey Treehouse",
-};
+// Setup Arrays
+const books = [
+  {
+    id: 1,
+    author: "by Steve Herman",
+    title: "Teach Your Dragon To Share",
+    img: "https://m.media-amazon.com/images/I/71f9a+-aSPL._AC_UL320_.jpg",
+    alt: "Teach Your Dragon To Share Book",
+  },
+  {
+    id: 2,
+    author: "by Andy Griffiths and Terry Denton",
+    title: "The 143-Storey Treehouse",
+    img: "https://m.media-amazon.com/images/I/91fUxKC9POL._AC_UL320_.jpg",
+    alt: "The 143-Storey Treehouse",
+  },
+  {
+    id: 3,
+    author: "by Emily Winfield Martin",
+    title: "The Wonderful Things You Will Be",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81opYgH1BJL._AC._SR360,460.jpg",
+    alt: "The Wonderful Things You Will Be",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book img={firsBook.img} title={firsBook.title} author={firsBook.author} alt={firsBook.alt}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id inventore magni ut animi exercitationem. Aspernatur officia asperiores eum expedita, in quasi deserunt ea dolorum atque rerum fugit? Possimus, doloremque voluptas!</p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} alt={secondBook.alt} />
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, alt, children }) => {
-  // const { img, title, author, alt } = props;
+const Book = (props) => {
+  const { img, title, author, alt } = props.book;
   return (
     <article className="book">
       <img src={img} alt={alt} />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
