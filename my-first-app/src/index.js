7df7e7773c0a4ffb579108b408b8bf05a4b84d71
Shohreh Nashvi <shohreh@i-motor.com.au>
2021-11-19 14:51:39 +1100

    My First React App - Add buttons with eventHander, OnClick and onMouseOver events.

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -39,13 +39,33 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author, alt } = props;
+const Book = ({ img, title, author, alt }) => {
+  // attribute, eventHandler
+  //OnClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("Button Clicked");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt={alt} />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More Complex Example
+      </button>
     </article>
   );
 };
